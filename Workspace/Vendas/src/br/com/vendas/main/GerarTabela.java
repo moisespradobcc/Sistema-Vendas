9e0cbdd4d1c8010e0e2f8548e7ca614d090aa885
moisespradobcc <moises.prado.bcc@gmail.com>
2022-05-05 23:05:37 -0300

    Criação do pacote domain e da classe Fornecedor.java.

---
+++ Workspace/Vendas/src/br/com/vendas/main/GerarTabela.java
@@ -0,0 +1,17 @@
+package br.com.vendas.main;
+
+import br.com.vendas.util.HibernateUtil;
+
+public class GerarTabela {
+
+	public static void main(String[] args) {
+
+
+		HibernateUtil.getSessionFactory();
+		HibernateUtil.getSessionFactory().close();
+		
+		
+
+	}
+
+}
